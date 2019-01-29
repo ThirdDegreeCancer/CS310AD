c5794b4fae78034058a0b67df23e9d032c6c0165
Tyson Potter <potter.tahson@gmail.com>
2019-01-29 11:18:13 -0600

    Added Controls to the Camera

---
+++ Game/Assets/NewBehaviourScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    //public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    //private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= .1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x += .1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z += .1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z -= .1f;
+            this.transform.position = position;
+        }
+    }
+
+
+    // LateUpdate is called after Update each frame
+    // LateUpdate()
+    //{
+    // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+    //transform.position = player.transform.position + offset;
+
+    //}
+}
