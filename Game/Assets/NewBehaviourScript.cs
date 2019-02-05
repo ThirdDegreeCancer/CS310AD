6f576b0c2d33558449e61c593f820dc56ae8ef02
Tyson Potter <potter.tahson@gmail.com>
2019-02-05 11:16:18 -0600

    Added Camera Control(Broken)
    The Restrictions on the X axis are broken. Nothing else

--- Game/Assets/NewBehaviourScript.cs
+++ Game/Assets/NewBehaviourScript.cs
@@ -4,53 +4,64 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
-    //public GameObject player;       //Public variable to store a reference to the player game object
-
-
-    //private Vector3 offset;         //Private variable to store the offset distance between the player and camera
-
-    // Use this for initialization
     void Start()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        //offset = transform.position - player.transform.position;
+
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x -= .1f;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x += .1f;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
+        Move();
+
+    }
+
+
+    void Move()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        //Move Forward
+        if (Input.GetKey(KeyCode.W))
         {
             Vector3 position = this.transform.position;
             position.z += .1f;
             this.transform.position = position;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        //Move Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= .1f;
+            this.transform.position = position;
+        }
+        //Move Back
+        if (Input.GetKey(KeyCode.S))
         {
             Vector3 position = this.transform.position;
             position.z -= .1f;
             this.transform.position = position;
         }
+        //Move Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x += .1f;
+            this.transform.position = position;
+        }
+        //Look Around
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+        transform.Rotate(-v, h, 0);
+        float z = transform.eulerAngles.z;
+        transform.Rotate(0, 0, -z);
+        if(this.transform.rotation.eulerAngles.x > 18)
+        {
+            this.transform.Rotate(-transform.rotation.x +18, 0, 0);
+        }
+        if (this.transform.rotation.eulerAngles.x < -18)
+        {
+            this.transform.Rotate(-transform.rotation.x -18, 0, 0);
+        }
     }
-
-
-    // LateUpdate is called after Update each frame
-    // LateUpdate()
-    //{
-    // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-    //transform.position = player.transform.position + offset;
-
-    //}
+   
+    
 }
